9ff4291e1792a39944b2d7bce0881553cc0249cf
Son0nline <son0nline@yahoo.com.vn>
2022-10-19 09:57:53 +0700

    update

---
+++ practice/les001_colection.py
@@ -0,0 +1,12 @@
+items = {
+    1: {
+        "name": "chair",
+        "price": 15.99
+    },
+    2: {
+        "name": "desk",
+        "price": 25.99
+    }, }
+
+print(items.values())
+print(items[1])
